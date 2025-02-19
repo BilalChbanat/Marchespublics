64a9b7fb8dd35a894dfdb10f96d04b1fb7261029
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-02-19 12:22:33 +0100

    init

---
+++ src/main/java/com/project/marchespublics/MarchespublicsApplication.java
@@ -0,0 +1,13 @@
+package com.project.marchespublics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchespublicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchespublicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/marchespublics/MarchespublicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.marchespublics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarchespublicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
