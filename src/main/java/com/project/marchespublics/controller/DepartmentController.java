8395b329ef1626be02555398d78ee89cd30ff690
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-03-05 23:24:53 +0000

    Department controller

---
+++ src/main/java/com/project/marchespublics/controller/DepartmentController.java
@@ -0,0 +1,48 @@
+package com.project.marchespublics.controller;
+
+
+import com.project.marchespublics.dto.DepartmentDto;
+import com.project.marchespublics.service.implementation.DepartmentServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/departments")
+@RequiredArgsConstructor
+public class DepartmentController {
+
+    private final DepartmentServiceImpl departmentService;
+
+    @PostMapping("/save")
+    public ResponseEntity<DepartmentDto> createDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto createdDepartment = departmentService.save(departmentDto);
+        return ResponseEntity.ok(createdDepartment);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DepartmentDto>> getAllDepartments(Pageable pageable) {
+        Page<DepartmentDto> allDepartments = departmentService.findAll(pageable);
+        return ResponseEntity.ok(allDepartments);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DepartmentDto> updateDepartment(@PathVariable Long id, @RequestBody DepartmentDto departmentDto) {
+        departmentDto.setId(id);
+        DepartmentDto updatedDepartment = departmentService.update(departmentDto);
+        return ResponseEntity.ok(updatedDepartment);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDepartment(@PathVariable int id) {
+        departmentService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable int id) {
+        return departmentService.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+}
